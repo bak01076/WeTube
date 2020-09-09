abbbb5e0302dc63a16b2dc873fd4e05ba1087944
Jongok Yoon <bak01076@gmail.com>
2020-09-09 12:48:57 +0200

    "divde and conquer(mvc) controller"

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Wetube
 
 cloning utube with VanillaJS and nodeJS
+
+
+
+MVC 패턴(구조)
+M(Model): data(데이터)
+V(View): how does the data look(데이터가 어떻게 생겼는지)
+C(Controller): function that looks for the data(데이터를 보여주는 함수)
--- app.js
+++ app.js
@@ -3,28 +3,23 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import {
-  userRouter
-} from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
-const handleHome = (req, res) => res.send('Hello from home');
-
-function handleProfile(req, res) {
-  res.send('Hello from profile');
-}
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(helmet());
-app.use(morgan('dev')); //middleware
+app.use(morgan('dev')); //middleware 
 
-app.get('/', handleHome);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.get('/profile', handleProfile);
-
-app.use("/user", userRouter);
 export default app;
