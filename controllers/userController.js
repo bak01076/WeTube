6c9d54ad2c2fbb7c8b3378f02d369bf4a524353e
Jongok Yoon <bak01076@gmail.com>
2020-09-29 13:54:49 +0200

    scss adapted

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-import routes from '../routes';
+import routes from "../routes";
 
 export const getJoin = (req, res) => {
-  res.render('join', { pageTitle: 'Join' });
+  res.render("join", { pageTitle: "Join" });
 };
 
 export const postJoin = (req, res) => {
@@ -10,7 +10,7 @@ export const postJoin = (req, res) => {
   } = req;
   if (password !== password2) {
     res.status(400);
-    res.render('join', { pageTitle: 'Join' });
+    res.render("join", { pageTitle: "Join" });
   } else {
     // To Do: Register User
     // To Do: Log user in``
@@ -19,7 +19,7 @@ export const postJoin = (req, res) => {
 };
 
 export const getLogin = (req, res) =>
-  res.render('login', { pageTitle: 'Log In' });
+  res.render("login", { pageTitle: "Log In" });
 export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
@@ -30,8 +30,8 @@ export const logout = (req, res) => {
 };
 
 export const userDetail = (req, res) =>
-  res.render('userDetail', { pageTitle: 'User Detail' });
+  res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
-  res.render('editProfile', { pageTitle: 'Edit Profile' });
+  res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
-  res.render('changePassword', { pageTitle: 'Change Password' });
+  res.render("changePassword", { pageTitle: "Change Password" });
--- init.js
+++ init.js
@@ -1,12 +1,12 @@
-import './db';
-import dotenv from 'dotenv';
-import app from './app';
-import './models/Video';
-import './models/Comment';
+import "./db";
+import dotenv from "dotenv";
+import app from "./app";
+import "./models/Video";
+import "./models/Comment";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const handleListening = () =>
   console.log(`✅  Listening on: http://localhost:${PORT}`);
